6d30f2a8351f10fd9f3d512344cd9856fe3e5985
Madison Fierro <madisonmfierro@gmail.com>
2018-05-29 14:46:36 -0700

    Debugged

--- app4/src/App.js
+++ app4/src/App.js
@@ -5,25 +5,37 @@ import axios from 'axios';
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       cars: []
     }
+    this.getCars = this.getCars.bind(this);
   }
 
   getCars() {
-    axios.get('https://joes-autos.herokuapp.com/api/vehicles')
-    .then(res => {
+    axios.get('https://joes-autos.herokuapp.com/api/vehicles').then(response => {
       this.setState({
-        cars: res
+        cars: response.data
       })
+      console.log(this.state.cars);
     })
   }
 
   render() {
+    const carsList = this.state.cars.map( list => {
+      return (
+        <div key={ list.id }>
+          <p>Make: {list.make} </p>
+          <p>Model: {list.model}</p>
+          <p>Year: {list.year}</p>
+          <p>Color: {list.color}</p>
+        </div>
+      )
+    })
     return (
-      <div className="App">
-        <button onClick={this.getCars}>Get cars</button>
-        {this.state.cars}
+      <div className={this.props.cars}>
+        <button onClick={ this.getCars }>Get cars</button>
+        {carsList}
       </div>
     );
   }
